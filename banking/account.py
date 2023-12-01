8ff1453204c6f2432e4e0e0be7167e971f8234f3
RiazulRahat <85854559+RiazulRahat@users.noreply.github.com>
2023-11-30 20:38:41 -0500

    Account class update
    Updated comments to make file easier to READ
    
    #NO CODES UPDATED

--- banking/account.py
+++ banking/account.py
@@ -1,49 +1,82 @@
-# Contains the structure of Account class
+#-------------------------------------------------------------------------------------#
+### NOTES ###
+#-------------------------------------------------------------------------------------#
+# 1# Contains the structure of Account class
 
-# Account class
+#-------------------------------------------------------------------------------------#
+### INCLUDES ###
+#-------------------------------------------------------------------------------------#
 from .transaction_log import TransactionLog
 from .transaction import Transaction
 
-
+#-------------------------------------------------------------------------------------#
+### ACCOUNT CLASS ###
+#-------------------------------------------------------------------------------------#
 class Account:
 
-    # initializer
+    #---------------------------------------------------------------------------------#
+    ### INITIALIZER ###
+    #---------------------------------------------------------------------------------#
+
     def __init__(self, account_name, deposit = 0.0):
-        self.account_name = account_name
+        self.account_name = account_name    # Necessary parameter
         self.balance = deposit
-        self.transaction_log = TransactionLog()
+        self.transaction_log = TransactionLog() # Account's Transaction Log
 
-    # deposit amount to account
+    #---------------------------------------------------------------------------------#
+    ### DEPOSIT ###
+    #---------------------------------------------------------------------------------#
+
     def deposit(self, amount, category = "Income"):
-        self.balance += amount
-        depTran = Transaction(amount, "Deposit", category)    # add transaction class
-        self.transaction_log.add_transaction(depTran)   # add transaction to self transactionLog
+        self.balance += amount  # ADD deposit amount to account balance
+        depTran = Transaction(amount, "Deposit", category)    # CREATE new Transaction
+        self.transaction_log.add_transaction(depTran)   # ADD Transaction to TransactionLog
 
-    # withdraw amount from account
+    #---------------------------------------------------------------------------------#
+    ### WITHDRAW ###
+    #---------------------------------------------------------------------------------#
+
     def withdraw(self, amount, category = "General"):
-        # check if withdrawing more than funds available
-        if amount <= self.balance:
-            self.balance -= amount
-            witTran = Transaction(amount, "Withdraw", category)   # add transaction class
-            self.transaction_log.add_transaction(witTran)   # add transaction to self transactionLog
+        # CHECK if balance available to Withdraw
+        if amount <= self.balance:  # TRUE
+            self.balance -= amount  # SUBTRACT withdraw amount
+            witTran = Transaction(amount, "Withdraw", category)   # CREATE new Transaction
+            self.transaction_log.add_transaction(witTran)   # ADD Transaction to TransactionLog
             print(f"Amount ${amount} withdrawn successfully.")
-        else:
+        else:   # FALSE
             print("Insufficient Funds...")
 
-    # transfer amount from self account, to another account class
+    #---------------------------------------------------------------------------------#
+    ### TRANSFER ###
+    #---------------------------------------------------------------------------------#
+
     def transfer(self, amount, to_account):
-        if amount <= self.balance:
-            self.balance -= amount
-            to_account.balance += amount
+        # CHECK if balance available to Transfer
+        if amount <= self.balance:  # TRUE
+            self.balance -= amount  # Take funds from THIS ACCOUNT
+            to_account.balance += amount    #   DEPOSIT to OTHER ACCOUNT
+            #-------------------------------------------------------------------------#
+            ### ADD TRANSACTION TO BOTH ACCOUNTS ###
+            #-------------------------------------------------------------------------#
             self.transaction_log.add_transaction(Transaction(amount, "Transfer In", "Transfer"))
             to_account.transaction_log.add_transaction(Transaction(amount, "Transfer Out", "Transfer"))
+
+            #-------------------------------------------------------------------------#
             print(f"Amount ${amount} transferred successfully.")
-        else:
+        else:   # FALSE
             print("Insufficient Funds...")
     
-    # Print balance
+    #---------------------------------------------------------------------------------#
+    ### CURRENT BALANCE ###
+    #---------------------------------------------------------------------------------#
+
     def get_balance(self):
         return self.balance
     
+    #---------------------------------------------------------------------------------#
+    ### LIST OF TRANSACTIONS (PER ACCOUNT)###
+    #---------------------------------------------------------------------------------#
     def get_transactions(self, category = "None"):
         return self.transaction_log.get_transactions(category)
+    
+    #---------------------------------------------------------------------------------#
