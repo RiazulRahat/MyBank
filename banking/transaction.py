87c8fe6ec8e417d9776ebd90949227d188febb02
RiazulRahat <85854559+RiazulRahat@users.noreply.github.com>
2023-11-30 21:50:16 -0500

    Transaction Class changes
    Comments added to make it easier to read and edit.

--- banking/transaction.py
+++ banking/transaction.py
@@ -1,29 +1,47 @@
-# Transaction Cluster
+#-------------------------------------------------------------------------------------#
+### NOTES ###
+#-------------------------------------------------------------------------------------#
+# 1# Contains the structure of Transaction class
 
-
+#-------------------------------------------------------------------------------------#
+### INCLUDES ###
+#-------------------------------------------------------------------------------------#
 from datetime import datetime
 
-
-# Transaction class
+#-------------------------------------------------------------------------------------#
+### TRANSACTION CLASS ###
+#-------------------------------------------------------------------------------------#
 class Transaction:
-    # initializer (account has to be passed everytime)
+    #---------------------------------------------------------------------------------#
+    ### INITIALIZER ###
+    #---------------------------------------------------------------------------------#
     def __init__(self, amount, tranType, category = "Other", date = None):
         self.amount = amount
         self.tranType = tranType
         self.category = category
-        self.date = datetime.now() if date is None else date
-        
+        self.date = datetime.now() if date is None else date    # Set current date if no date provided
+    
+    #---------------------------------------------------------------------------------#
+    ### OUTPUT STRING ###
+    #---------------------------------------------------------------------------------#
     def __str__(self):
         print(f"Transaction : {self.tranType} of ${self.amount} on {self.date} \nCategory: {self.category}")
 
-
+    #---------------------------------------------------------------------------------#
+    ### CHECK IF TRANSACTION TYPE IS VALID ###
+    #---------------------------------------------------------------------------------#
     def is_Valid(self):
         return self.amount > 0 and self.tranType in ["Deposit", "Withdraw", "Transfer In", "Transfer Out"]
     
 
-    # returns string "amount tranType date" for storage
+    #---------------------------------------------------------------------------------#
+    ### RETURN STRING {AMOUNT TRANTYPE CATEGORY DATE} IN THIS FORMAT ###
+    #---------------------------------------------------------------------------------#
     def serialize(self):
         return f"{self.amount} {self.tranType} {self.category} {self.date}"
     
+    #---------------------------------------------------------------------------------#
+    ### RETURN CATEGORY ###
+    #---------------------------------------------------------------------------------#
     def get_category(self):
         return self.category
