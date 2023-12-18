6dcced74715513b0728f617b3bf0354e23f2f9ef
RiazulRahat <85854559+RiazulRahat@users.noreply.github.com>
2023-12-17 22:15:17 -0500

    BugFix
    Fixed more CLI bugs and also established some better UI.

--- UI/cli.py
+++ UI/cli.py
@@ -5,8 +5,7 @@ from banking.account import Account
 accounts = {}
 
 def create_account_ui():
-    print("Create New Account")
-    print("---------------------")
+    print(text_center("Create New Account"))
 
     account_name = input("Enter Account Name: ")
     initial_deposit = float(input("Initial Deposit Amount: "))
@@ -15,11 +14,11 @@ def create_account_ui():
 
     accounts[account_name] = new_account
 
-    print(f"Account created successfully! \nAccount Name: {account_name}\nInitial balance: {initial_deposit}\n")
-
-    return account_name
+    print(text_center(f"Account created successfully! \n\nAccount Name: {account_name}\nInitial balance: {initial_deposit}"))
 
 def deposit_ui(account_name):
+    print(text_center("DEPOSIT"))
+
     amount = float(input("Enter Deposit Amount: "))
     if(input("Input Category? Y/N: ") == "Y"):
         category = input("Category: ")
@@ -29,8 +28,10 @@ def deposit_ui(account_name):
     accounts[account_name].deposit(amount, category)
     print(f"Amount ${amount} deposited successfully.")
 
-def withdraw_ui(account_name):
+    end_print()
 
+def withdraw_ui(account_name):
+    print(text_center("WITHDRAW"))
     amount = float(input("Enter Withdraw Amount: "))
     if(amount <= 0):
         print("Invalid Withdraw Amount!\n")
@@ -48,15 +49,13 @@ def withdraw_ui(account_name):
         category = "General"
         
     accounts[account_name].withdraw(amount, category)
-    print(f"Amount ${amount} withdrawn successfully.")
+    end_print()
 
 def transfer_ui(transfer_from):
-    print("Transferring From \n")
-    print("----------------------")
+    print(text_center("Transferring From"))
     print(f"Account Name: {transfer_from}")
     amount = float(input("Enter Amount to Transfer: "))
-    print("Transferring To \n")
-    print("----------------------")
+    print(text_center("Transferring To"))
     transfer_to = input("Account Name: ")
 
     if verifyAccount(transfer_to):
@@ -64,15 +63,20 @@ def transfer_ui(transfer_from):
     else:
         print("Invalid Account to Transfer!")
 
+    end_print()
+
 def current_balance(account_name):
     balance = accounts[account_name].get_balance()
-    print(f"Current Balance = ${balance}")
+    print(text_center(f"Current Balance = ${balance}"))
 
 def get_all_transactions(account_name):
-    print("\n" + accounts[account_name].get_transactions())
+    print(text_center("TRANSACTION HISTORY"))
+    print(accounts[account_name].get_transactions())
+    current_balance()
 
 def main_menu(account_name):
     while verifyAccount(account_name):
+        print(text_center("MAIN MENU"))
         print(f"Account Name: {account_name}\n1: Deposit \n2: Withdraw \n3: Transfer \n4: Current Balance \n5: Show all transactions \n6: Switch Accounts \n7: Log Out")
         choice = int(input("Enter your choice: "))
 
@@ -95,23 +99,30 @@ def main_menu(account_name):
 
 # Make this the [default UI] - then this account name gets passed to every functions
 def login_screen():
+    account_name = ""
     while True:
-        print("Log In To Your Account \n")
-        print("--------------------------")
+        print(text_center("Log In To Your Account"))
 
         #Add password definitions later#
 
         account_name = input("Enter your Account Name: ")
         if(verifyAccount(account_name)):
+            print(f"Welcome Back {account_name}")
             action = main_menu(account_name)
             if (action == "logout"):
+                exit()
                 break
+
+            # if user wants to swtch accounts, go thru login_screen again
+            elif (action == "switch"):
+                print(f"See you again {account_name}!")
+                continue
     
         else:
-            print("Account not Found! \n")
+            print("Account not Found!")
             if(input("Register New Account? Y/N : ") == "Y"):
-                account_name = create_account_ui()
-                main_menu(account_name)
+                create_account_ui()
+                continue
             else:
                 exit()
 
@@ -124,4 +135,19 @@ def verifyAccount(account_name):
 
 
 def exit():
+    end_print()
     print("\n\n\nThank you for using MyBank!\n\n\n")
+    end_print()
+
+
+
+def text_center(text):
+    text = f"--------------------------\n{text}\n--------------------------"
+    return text
+
+def text_top(text):
+    text = f"{text}\n--------------------------"
+    return text
+
+def end_print():
+    print("--------------------------")
